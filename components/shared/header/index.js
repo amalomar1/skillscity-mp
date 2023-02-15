a8d423f26cb129d038e18367c02b47ac0165f79d
Amal Omar <amalomar1995@hotmail.com>
2023-02-15 19:07:20 +0000

    created a different colour for text

--- components/shared/header/index.js
+++ components/shared/header/index.js
@@ -1,6 +1,6 @@
 export default function Header({ name }) {
     return (
-        <div className="bg-amber-100 w-full max-w-6xl mx-auto py-4 px-4">
+        <div className="bg-amber-100 text-amber-900 w-full max-w-6xl mx-auto py-4 px-4">
             <h1 className="font-bold text-4xl">{name} Page</h1>
         </div>
     );
