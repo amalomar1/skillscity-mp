3365785063f4966b3b916996701b2dc614c166ec
Amal Omar <amalomar1995@hotmail.com>
2023-02-15 18:55:47 +0000

    created a different colour for text

--- components/contact/list-of-messages/index.js
+++ components/contact/list-of-messages/index.js
@@ -2,9 +2,9 @@ export default function ListOfMessages({ isLoading, messages }) {
     if (isLoading) {
         return (
             <div className="w-full grid grid-cols-3 gap-4 mt-10">
-                <div className="bg-gray-200 py-3 px-6 rounded h-28 animate-pulse" />
-                <div className="bg-gray-200 py-3 px-6 rounded h-28 animate-pulse" />
-                <div className="bg-gray-200 py-3 px-6 rounded h-28 animate-pulse" />
+                <div className="bg-amber-100 py-3 px-6 rounded h-28 animate-pulse" />
+                <div className="bg-amber-100 py-3 px-6 rounded h-28 animate-pulse" />
+                <div className="bg-amber-100 py-3 px-6 rounded h-28 animate-pulse" />
             </div>
         )
     }
@@ -12,7 +12,7 @@ export default function ListOfMessages({ isLoading, messages }) {
     return (
         <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
             {messages.map((message, index) => (
-                <div key={index} className="bg-gray-200 py-3 px-6 rounded">
+                <div key={index} className="bg-amber-100 py-3 px-6 rounded">
                     <p className="font-medium text-xl">{message.name}</p>
                     <p>
                         {message.email ? message.email : (
