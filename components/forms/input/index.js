b29fb3775a083bcd4f220d61ea8cda8fb5acf2f5
Amal Omar <amalomar1995@hotmail.com>
2023-02-15 18:56:43 +0000

    created a different colour for text

--- components/forms/input/index.js
+++ components/forms/input/index.js
@@ -7,7 +7,7 @@ export default function Input(props) {
     return (
         <input
             {...props}
-            className="bg-pink-200 py-3 px-6 w-96 rounded"
+            className="bg-amber-100 py-3 px-6 w-96 rounded"
             onChange={handleChange}
         />
     )
