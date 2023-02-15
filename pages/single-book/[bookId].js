87c289639bf800a52814478c2d24d1a3482c0ef3
Amal Omar <amalomar1995@hotmail.com>
2023-02-15 18:45:56 +0000

    created a different colour for text

--- pages/single-book/[bookId].js
+++ pages/single-book/[bookId].js
@@ -49,7 +49,7 @@ export default function SingleBook() {
                     <div className="">
                         <CopyToClipboard text={book.linkToPurchase}>
                             <button
-                                className="bg-blue-500 text-white px-4 py-2 rounded-lg"
+                                className="bg-amber-600 text-white px-4 py-2 rounded-lg"
                                 type="button"
                             >
                                 Copy link to Amazon
