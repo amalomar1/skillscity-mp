9060910c607ac5c83ee07ba06d3a1b54ba4cf235
Amal Omar <amalomar1995@hotmail.com>
2023-02-15 16:10:00 +0000

    (px-3)include horizontal padding to make things better to use on smaller screens

--- pages/contact.js
+++ pages/contact.js
@@ -1,14 +1,36 @@
+import ListOfMessages from "@/components/contact/list-of-messages";
 import Input from "@/components/forms/input";
 import Content from "@/components/shared/content";
 import Footer from "@/components/shared/footer";
 import Header from "@/components/shared/header";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Contact() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
     const [showSuccess, setShowSuccess] = useState(false);
+    const [messages, setMessages] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        getMessages();
+    }, []);
+
+    const getMessages = async () => {
+        setIsLoading(true);
+        const response = await fetch(`/api/contact-messages`);
+        const data = await response.json();
+        
+        const { messages } = data;
+
+        console.log(messages);
+
+        setMessages(messages);
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 3000);
+    }
 
     const handleChangeMessage = (event) => {
         const message = event.target.value;
@@ -23,6 +45,7 @@ export default function Contact() {
         setEmail("");
         setMessage("");
         setShowSuccess(true);
+        getMessages();
     }
 
     return (
@@ -30,12 +53,11 @@ export default function Contact() {
             <Header name="Contact" />
 
             <Content>
-                <form className="flex flex-col space-y-3">
+                <form className="flex flex-col space-y-3 w-full sm:w-96 px-3">
                     <Input
                         placeholder="Name"
                         value={name}
                         onChange={(value) => setName(value)}
-                        disabled={true}
                     />
                     <Input
                         placeholder="Email"
@@ -44,13 +66,13 @@ export default function Contact() {
                     />
                     <textarea
                         placeholder="Message"
-                        className="bg-gray-200 py-3 px-6 w-96 rounded"
+                        className="bg-gray-200 py-3 px-6 w-full rounded"
                         value={message}
                         onChange={handleChangeMessage}
                     />
 
                     <button
-                        className="bg-blue-500 py-3 px-6 w-96 rounded text-white"
+                        className="bg-blue-500 py-3 px-6 w-full rounded text-white"
                         onClick={handleSubmit}
                         type="button"
                     >
@@ -58,13 +80,15 @@ export default function Contact() {
                     </button>
 
                     {showSuccess === true ? 
-                        (<p className="text-green-500 w-96">
+                        (<p className="text-green-500 w-full">
                             Your message was sent!
                         </p>)
                         :
                         null
                     }
                 </form>
+
+                <ListOfMessages isLoading={isLoading} messages={messages} />
             </Content>
 
             <Footer title="Home" href="/" />
