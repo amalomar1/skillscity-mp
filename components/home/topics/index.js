f503e9156c14bcf317f9127ec9e1c526afd86b89
Amal Omar <amalomar1995@hotmail.com>
2023-02-15 19:16:12 +0000

    created a different colour

--- components/home/topics/index.js
+++ components/home/topics/index.js
@@ -3,8 +3,8 @@ import topics from "./topics";
 export default function Topics() {
     return (
         <div className="w-full flex flex-col mt-5 mb-10">
-            <h3 className="font-medium text-2xl">
-                Topics covered in this course
+            <h3 className="font-medium text-amber-900 text-2xl">
+                Topics covered in this course:
             </h3>
             <div>
                 <ul className="list-disc">
