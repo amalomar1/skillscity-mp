1e6e1935d1b957121ee0be364a615ebefb23a406
Amal Omar <amalomar1995@hotmail.com>
2023-02-15 19:09:22 +0000

    created a different colour for text

--- components/locations/locations-grid/location-item.js
+++ components/locations/locations-grid/location-item.js
@@ -4,7 +4,7 @@ export default function LocationItem({ location }) {
     return (
         <div className="border rounded-lg p-4">
             <img src={location.imgUrl} className="rounded-lg" />
-            <p className="text-xl font-medium">{location.title}</p>
+            <p className="text-xl font-medium text-amber-900">{location.title}</p>
             <p className="text-amber-700">{location.author}</p>
             <LikeButton />
         </div>
